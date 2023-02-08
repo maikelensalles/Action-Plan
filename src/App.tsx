3f2034b1c0f64cb1a8fd83923aa47e2ff4d26982
Maikelen Salles <maiki09salles@gmail.com>
2023-02-08 14:42:59 -0300

    modo dark

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 //import './App.css';
 
-
-
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 function App() {
