c0a29d8e07504734052a097d4e3357ce2f721a03
Maikelen Salles <maiki09salles@gmail.com>
2023-02-10 16:36:04 -0300

    estrutura pagina Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,15 @@
 import React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-//import './App.css';
-
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home';
 
 function App() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      {theme.palette.mode} mode
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter> 
   );
 }
 
-export default function ToggleColorMode() {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-}
+export default App;
