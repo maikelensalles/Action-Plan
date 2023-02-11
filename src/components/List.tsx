14b7ee66686a8b0c87cd14f5c5ab44cc56573c7b
Maikelen Salles <maiki09salles@gmail.com>
2023-02-11 16:30:26 -0300

    limpar TextField

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,86 +1,35 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
-import './list.css';
 import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import './list.css';
 
 export default function CheckboxList() {
 
   const [value, setValue] = useState<string>("");
   const [todos, setTodos] = useState<string[]>([]);
   
-  const [checked, setChecked] = React.useState([0]);
-
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
   return (
-    <div className="list">
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <form onSubmit={event => {
-          event.preventDefault();
-          if (!!value) {
-            setTodos([...todos, value]);
-          }
-          return;
-        }}>
-          <input
-            type="text"
-            value={value}
-            onChange={event => setValue(event.target.value)}
-          />
-          <button type='submit'>Adicionar</button>
-        </form>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>{todo}</li>
-          ))}
-        </ul>
-        {[0, 1, 2, 3].map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
-
-          return (
-            <ListItem
-              key={value}
-              secondaryAction={
-                <IconButton edge="end" aria-label="comments">
-                  <CommentIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.indexOf(value) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ 'aria-labelledby': labelId }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
+    <div className="list-container">
+        <div className="container">
+          <form onSubmit={event => {
+            event.preventDefault();
+            if (!!value) {
+              setTodos([...todos, value]);
+              setValue("");
+            }
+            return;
+          }}>
+            <TextField label="Tarefa" variant="outlined" type="text"
+              value={value}
+              onChange={event => setValue(event.target.value)} />
+            <button type='submit'>Adicionar</button>
+          </form>
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index}>{todo}</li>
+            ))}
+          </ul>
+        </div>
     </div>
   );
 }
